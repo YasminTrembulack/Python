77f5c61eb431edeb010c0139e86f102005336463
YasminTrembulack <142899321+YasminTrembulack@users.noreply.github.com>
2023-08-30 13:26:14 -0300

    Add files via upload

---
+++ Yasmin Trembulack Agostinho/Aula 2 Python (25_08).py
@@ -0,0 +1,78 @@
+    #============Exercício 3============
+# nome_completo = input("Digite seu nome completo com letras minúsculas:")
+# nomeMaiusculo = nome_completo.title()
+# nomeContagem = nomeMaiusculo.replace(" ","")
+# print(nomeMaiusculo, "\nSeu nome tem {} letras.".format(len(nomeContagem)))
+
+# yasmin trembulack agostinho
+
+
+#     Ordenar lista
+# lista = [4, 8, 2, 3, 1]
+# lista.sort()
+# print(lista)
+
+#     Subistituir valor da lista
+# lista[0] = "a"
+# print(lista)
+#     Ver se tem algo na lista
+# print(9 in lista)
+
+#     Concatenar lista
+# lista2 = ["YAS", "SLA"]
+# print(lista + lista2)
+
+#     #===========Exercício 4===========
+# soma = 0
+# listaNumeros = []
+# for i in range(10):
+#     soma += 1
+#     listaNumeros.append(int(input(f"Valor{soma}:")))
+#     listaNumeros.sort()
+# print(listaNumeros)
+#     ou
+# listaNumeros = []
+# listaNumeros.append(int(input(f"Valor1:")))
+# listaNumeros.append(int(input(f"Valor2:")))    
+# listaNumeros.append(int(input(f"Valor3:")))    
+# listaNumeros.append(int(input(f"Valor4:")))    
+# listaNumeros.append(int(input(f"Valor5:")))    
+# listaNumeros.append(int(input(f"Valor6:")))    
+# listaNumeros.append(int(input(f"Valor7:")))    
+# listaNumeros.append(int(input(f"Valor8:")))    
+# listaNumeros.append(int(input(f"Valor9:")))    
+# listaNumeros.append(int(input(f"Valor10:")))
+# listaNumeros.sort()
+# print(len(listaNumeros))
+# print(listaNumeros)
+
+
+#     #=========Exercício 5==========
+# topRicos2023 = ("Elon Musk ($180 B)", "Jeff Bezos ($114 B)", "Larry Ellison ($107 B)", "Bill Gates ($104 B)",  "Warren Buffett ($106 B)", 
+# "Mark Zuckerberg ($ 112,8 B)", "Larry Page (S$ 112 B)", "Serguey Brin (S$ 106,3 B)", "Steve Ballmer ($102 B)")
+# print("Os 10 mais ricos são:", topRicos2023)
+# print("Os 3 mais ricos são:", topRicos2023[:3])
+# print("O mais rico do mundo é:", topRicos2023[0])
+
+#     #=========Exercício 6==========
+# total = 0
+# Menu = {"BatataFrita": 7 , "Hamburger": 12, "HotDog": 10, "Suco": 4, "Refrigerante": 5}
+# print(Menu)
+# n = int(input("Digite a quantidade de produtos que você deseja compra:"))
+# for i in range(n):
+#     comida = input("Digite a comida que deseja:")
+#     total += Menu[comida]
+# print(f"O valor total é de: R${total},00")
+     
+#     #=========Exercício 1==========
+# numeroSecreto = "42"
+# print("JOGO DA ADIVINHAÇAÕ", "\n------------------------------------------------")
+
+# numero = input("Digite seu palpite:")
+# letra = numero.isdigit()
+# if letra == False:
+#     print("Isso não é um número!  >:(")
+# elif numero == numeroSecreto:
+#     print("Parabéns!! Você acertou :)")
+# else:
+#     print("Você errou. :(")  
---
+++ Yasmin Trembulack Agostinho/Aula 3 Python (28_08).py
@@ -0,0 +1,148 @@
+    #=========Exercício 2==========
+
+# ano_atual = int(input("Digite o ano atual: "))
+# ano_nascimento = int(input("Digite seu ano de nascimento: "))
+# niver = int(input("Já fez aniversário esse ano? (1 - sim / 2 - não): "))
+
+# idade = ano_atual - ano_nascimento
+# idade2 = ano_atual - ano_nascimento - 1
+
+# if niver == 1:
+#     if idade < 16:
+#         print(f"Você tem {idade} anos.", "\nVocê não pode votar.")
+#     elif idade < 18:
+#         print(f"Você tem {idade} anos.", "\nSeu voto è facultativo!")
+#     elif idade > 70:
+#         print(f"Você tem {idade} anos.", "\nSeu voto è facultativo!")
+#     else:
+#         print(f"Você tem {idade} anos.", "\nVocê TEM que votar!")
+# else:
+#     if idade2 < 16:
+#         print(f"Você tem {idade2} anos.", "\nVocê não pode votar.")
+#     elif idade2 < 18:
+#         print(f"Você tem {idade2} anos.", "\nSeu voto è facultativo!")
+#     elif idade2 > 70:
+#         print(f"Você tem {idade2} anos.", "\nSeu voto è facultativo!")
+#     else:
+#         print(f"Você tem {idade2} anos.", "\nVocê TEM que votar!")
+
+    #=========Exercício 3==========
+
+# limite = int(input("Digite o limite: "))
+# n = 0
+# total = 0
+# while n < limite:
+#     n += 1
+#     total += n
+#     print(f"{n} +")
+# print(f"O valor total da soma é: {total}")
+
+    #=========Exercício 4==========
+
+# while True:
+#     numero = float(input("Digite um número: "))
+#     numero2 = float(input("Digite outro número: "))
+#     print("Digite:", "\n1 - Soma", "\n2 - Subtração", "\n3 - Multiplicação", "\n4 - Divisão", "\n0 - Sair.")
+#     operacao = int(input("Digite a operação desejada: "))
+#     if operacao == 0:
+#         print("Sessão finalizada!")
+#         break
+#     elif operacao == 1:
+#         soma = numero + numero2
+#         print(f"O resultado da soma é: {soma}")
+#     elif operacao == 2:
+#         subtracao = numero - numero2
+#         print(f"O resultado da subtracao é: {subtracao}")
+#     elif operacao == 3:
+#         multiplicacao = numero * numero2
+#         print(f"O resultado da multiplicacao é: {multiplicacao}")
+#     elif operacao == 4:
+#         if numero2 == 0:
+#             print("Não é possível dividir por zero.")
+#         else:
+#             divisao = numero / numero2
+#             print(f"O resultado da divisao é: {divisao}")
+#     parar = input("Digite 0 se quiser sair. ")
+#     if parar == "0":
+#         print("Sessão finalizada!")
+#         break
+
+     #=========Exercício 5==========
+# for numeros in range(31):
+#     if numeros % 2 == 0:
+#         print(numeros)
+        
+#for n in range(0, 31, 2):
+#    print(n)
+        
+    #=========Exercício 6==========
+# fileira = int(input("Quantas fileiras? "))
+# coluna = int(input("Quantas colunas? "))
+
+# for i in range(fileira):
+#     for j in range(coluna):
+#         print("X ", end="")
+#     print()
+
+     #=========Exercício 7==========
+# limite = int(input("Digite o limite: "))
+# anterior = 0 
+# proximo = 1
+# soma = 0 
+# for i in range(limite):
+#     print(soma)
+#     soma = proximo + anterior
+#     proximo = anterior
+#     anterior = soma
+#     #ou
+# qtd = int(input("> "))
+# lista = [0, 1]
+
+# for i in range(2, qtd):
+#     lista.append(lista[i-1] + lista[i-2])
+# print(lista[:qtd])
+
+     #=========Exercício 8==========
+# div =[]
+# num = int(input("Digite um número: "))
+# for i in range(1, (num + 1)):
+#     if num % i == 0:
+#         div.append(i)
+        
+# if len(div) > 3:
+#     print("Não é primo")
+# else:
+#     print("É primo")
+# print(div)
+
+ #=========Exercício 9==========
+
+import random
+
+RECORDE = 0
+while True:
+    Vitórias = 0
+    x = random.randint(0,101)
+    jogador_num = int(input("Digite um número: "))
+    par_impar = input("PAR OU IMPAR? ")
+    soma = x + jogador_num
+    if (soma % 2 ==0 and par_impar == "par") or (soma % 2 != 0 and par_impar == "impar"):
+        Vitórias += 1
+        if Vitórias > RECORDE:
+            RECORDE = Vitórias
+        print(f"O computador jogou: {x}")
+        print("Você venceu!")
+        print(f"\nVitórias = {Vitórias}")
+        print(f"\nRECORDE = {RECORDE}")
+    elif (soma % 2 == 0 and par_impar == "impar") or (soma % 2 != 0 and par_impar == "par"):
+        print(f"O computador jogou: {x}")
+        print("Você perdeu!")
+        print(f"\nVitórias = {Vitórias}")
+        print(f"\nRECORDE = {RECORDE}")
+    parar = input("Digite 0 se quiser sair. ")
+    if parar == "0":
+        print(f"\nRECORDE = {RECORDE}")
+        print("Jogo finalizado!")
+        break
+
+        
