3da8962aae6b5279f97dfe879e8e3d582b5d8723
YasminTrembulack <142899321+YasminTrembulack@users.noreply.github.com>
2023-09-13 16:37:06 -0300

    Update Aula 7 Python (12_09).py

--- Aula 7 Python (12_09).py
+++ Aula 7 Python (12_09).py
@@ -2,7 +2,7 @@
 #     def __init__(self, *args):
 #         if len(args)>3:
 #             print("Inserindo alunos")
-#         elif len(args)==1:  ou isinstance(args)==1:
+#         elif len(args)==1:  #ou isinstance(args)==1:
 #             print("Inserindo turma")
 # algo = ETS("Turma nova", 1, 2, 3, 4)
 
@@ -10,14 +10,7 @@
 
 
 
-
-#Implemente um programa no qual o usuário deverá informar o nome e a idade de três pessoas. O programa deverá informar o nome da pessoa que possuir a maior idade.
-# Regras que deverão ser seguidas para a implementação do algoritmo:
-# •	É obrigatório o uso de classe para representar uma pessoa e a mesma deverá possuir como propriedades (características) um nome e uma idade.
-# •	A classe também deverá possuir um método chamado ExibirDados. Esse método deverá exibir o nome e a idade da pessoa em questão.
-# •	Ao implementar a classe é obrigatório implementar dois construtores (Sobrecarga), um que não recebe parâmetro algum e outro que irá receber o nome e a idade de uma pessoa.
-
-#========== Atividede 1 ==========
+# #========== Atividede 1 ==========
 
 # class Pessoa:
 #     def __init__(self, *args):
@@ -82,56 +75,158 @@
 
 
 # class Animal:
-#     def __init__(self, nome, tipo, qntg = 0, qntc = 0, qntp = 0):
-#         Animal.gato = 0
-#         Animal.cachorro = 0
-#         Animal.peixe = 0
+#     def __init__(self, nome, tipo):
+#         Animal.gato = []
+#         Animal.cachorro = []
+#         Animal.peixe = []
 #         self.__tipo = tipo
 #         self.__nome = nome
-
-#         if self.__tipo == "gato":
-#             Animal.gato += 1
-#         elif self.__tipo == "cachorro":
-#             Animal.cachorro += 1
-#         else:
-#             Animal.peixe += 1
-        
         
 #     def exibir(self):
-#         print(f"O nome do {self.__tipo} é {self.__nome} e tem {Animal.gato} do seu tipo.")
+#         print(f"O nome do {self.__tipo} é {self.__nome} e tem {len(Animal.gato)} do seu tipo.")
+#         print(Animal.gato)
+#     def cat(self):
+#         if self.__tipo == "gato":
+#             Animal.gato.append(self.__nome)
+#     def dog(self):
+#         if self.__tipo == "cachorro":
+#             Animal.cachorro.append(self.__nome)
+#     def fish(self):
+#         if self.__tipo == "peixe":
+#             Animal.peixe.append(self.__nome)
+
 
         
 # gato1 = Animal("sla", "gato")
-# gato3 = Animal("sla", "gato")
-# gato4 = Animal("sla", "gato")
-# gato2 = Animal("aaaaa", "gato")
+# gato1.cat()
+
+# gato2 = Animal("aaaa", "gato")
+# gato2.cat()
+# gato3 = Animal("bbb", "gato")
+# gato3.cat()
+
+# gato4 = Animal("ccccc", "gato")
+# gato4.cat()
 # gato4.exibir()
+# print(gato4.gato)
+
+
+
+#=================================
+
+#   ->    CORRETO
+
+# class Animal:
+#     def __init__(self, nome, tipo):
+#         self.__nome = nome
+
+#         if tipo == "cachorro":
+#             Animal.cachorro.append(nome)
+#             self.__tipo = tipo
+#         elif tipo == "gato":
+#             Animal.gato.append(nome)
+#             self.__tipo = tipo
+#         else:
+#             Animal.peixe.append(nome)
+#             self.__tipo = "peixe"
+
+#     @staticmethod
+#     def contabilizar(): #metodo estático
+#         Animal.peixe = []
+#         Animal.cachorro = []
+#         Animal.gato = []
+
+#     @staticmethod
+#     def mostrar(): #metodo estático
+#         print(f"Peixes: {len(Animal.peixe)} = {Animal.peixe}")
+#         print(f"Cachorros: {len(Animal.cachorro)} = {Animal.cachorro}")
+#         print(f"Gatos: {len(Animal.gato)} = {Animal.gato}")
+
+# Animal.contabilizar()
+# #inicia o atributo estatico para setar os valores inicias de cada tipo de animal
+# a1 = Animal("Garfield", "gato")
+# a2 = Animal("Marlin", "peixe")
+# a3 = Animal("Cezar", "macaco")
+# a4 = Animal("Tobby", "cachorro")
+# #depois ele define os objetos verifica qual o tipo no __init__ e adiciona +1 nos atributos já definidos anteriormente.
+# a1.mostrar()
+# #AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+
+#=================================
+# class Animal:
+#     def __init__(self, nome, tipo):
+#         self.__tipo = tipo
+#         self.__nome = nome   
+        
+#     def exibir(self):
+#         print(f"O nome do {self.__tipo} é {self.__nome} e tem {len(Animal.gato)} do seu tipo.")
+#         print(Animal.gato)
+#     def inf(self):
+#         return self.__tipo
+
+#     def dog(self):
+#         if self.__tipo == "cachorro":
+#             Animal.cachorro.append(self.__nome)
+#     def fish(self):
+#         if self.__tipo == "peixe":
+#             Animal.peixe.append(self.__nome)
+
+# class Tipos(Animal):
+#     def __init__(self, nome, tipo):
+#         super().__init__(nome, tipo)
+#         self.gato = []
+#         self.cachorro = []
+#         self.peixe = []
+
+#     def cadastrar(self):
+#         if  self.inf == "gato":
+#             self.gato.append(self.inf)
+        
+#     def mostrar(self):
+#         print(self.gato)
+
+
+# gato1 = Tipos("sla", "gato")
+# gato1.cadastrar()
+
+# gato2 = Tipos("aaaa", "gato")
+# gato2.cadastrar()
+
+# gato3 = Tipos("bbb", "gato")
+# gato3.cadastrar()
+
+# gato4 = Tipos("ccccc", "gato")
+# gato4.cadastrar()
+
+# gato4.mostrar()
+
 
 #========== Atividede 4 ==========
-import random
+# import random
 
-class Jogador :
-    def __init__(self, id: int, nome:str, apelido: str):
-        self.id = id
-        self.nome = nome
-        self.apelido = apelido
+# class Jogador :
+#     def __init__(self, id: int, nome:str, apelido: str):
+#         self.id = id
+#         self.nome = nome
+#         self.apelido = apelido
 
-class Plantel:
-    def __init__(self):
-        self.lista = []
+# class Plantel:
+#     def __init__(self):
+#         self.lista = []
 
-    def cadastrarJogadores(self, jogador):
-        self.lista.append(jogador)
-    def mostrar(self):
-        for i in range(len(self.lista)):
-            print(f"Nome: {self.lista[i].nome}" +
-                  f" e Apelido: {self.lista[i].apelido}")
+#     def cadastrarJogadores(self, jogador):
+#         self.lista.append(jogador)
+#     def mostrar(self):
+#         for i in range(len(self.lista)):
+#             print(f"Nome: {self.lista[i].nome}" +
+#                   f" e Apelido: {self.lista[i].apelido}")
 
 
-nome = input("Digite o nome: ")        
-apelido = input("Digite o apelido: ")
-j1 = Jogador(random.randint(0,1000), nome, apelido )
+# nome = input("Digite o nome: ")        
+# apelido = input("Digite o apelido: ")
+# j1 = Jogador(random.randint(0,1000), nome, apelido )
 
-p1 = Plantel()
-p1.cadastrarJogadores(j1)
-p1.mostrar()
+# p1 = Plantel()
+# p1.cadastrarJogadores(j1)
+# p1.mostrar()
